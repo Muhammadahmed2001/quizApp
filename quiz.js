25ae8a5cf0d81c88b9b27ffc5410034e3922952a
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-06-12 23:17:47 +0500

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,7 @@
+var question = {
+    question: 'What does HTML stand for?',
+    option1: 'Hyperlinks and Text Markup Language',
+    option2: 'Hypertext Markup Language',
+    option3: 'Home Tool Markup Language',
+    correctOption: "Hypertext Markup Language",
+}
